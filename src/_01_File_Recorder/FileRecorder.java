4cd4c69f1f17ce5383a899cf85e40750fc0266e9
codeavenger1 <shivakansagara@gmail.com>
2020-03-22 17:29:48 -0700

    finished file recorder; started file encryptor

--- src/_01_File_Recorder/FileRecorder.java
+++ src/_01_File_Recorder/FileRecorder.java
@@ -1,5 +1,21 @@
 package _01_File_Recorder;
 
+import java.io.FileWriter;
+import java.io.IOException;
+
+import javax.swing.JOptionPane;
+
 public class FileRecorder {
 	// Create a program that takes a message from the user and saves it to a file.
+	public static void main(String[] args) {
+		String answer = JOptionPane.showInputDialog("Enter a message");
+		
+		try {
+			FileWriter fw = new FileWriter("src/_00_Intro_To_File_Input_and_Output/FileRecorder.txt");
+			fw.write(answer);
+			fw.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 }
